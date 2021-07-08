59a90305143d565ae0ace3e2360d5005536c1b01
kmrshubham123 <kmrshubham123@gmail.com>
2021-07-08 20:32:21 +0530

    [Kumar Shubham]Refactor in Random Method

--- SequenceProblem/RandomDice.js
+++ SequenceProblem/RandomDice.js
@@ -8,10 +8,10 @@ console.log("Your Dice Number is: "+Dice);
 let Dice_One=Math.floor(Math.random()*6)+1;
 let Dice_Two=Math.floor(Math.random()*6)+1;
 console.log("First Dice Number is: "+Dice_One+ " "+"And Second Dice Number is : "+Dice_Two);
-//5 Random 2 digit number (*99)+10=>10 to 99 
-let NumOne=Math.floor(Math.random()*99)+10;
-let NumTwo=Math.floor(Math.random()*99)+10;
-let NumThree=Math.floor(Math.random()*99)+10;
-let NumFour=Math.floor(Math.random()*99)+10;
-let NumFive=Math.floor(Math.random()*99)+10;
+//5 Random 2 digit number (*90)+10=>10 to 99
+let NumOne=Math.floor(Math.random()*90)+10;
+let NumTwo=Math.floor(Math.random()*90)+10;
+let NumThree=Math.floor(Math.random()*90)+10;
+let NumFour=Math.floor(Math.random()*90)+10;
+let NumFive=Math.floor(Math.random()*90)+10;
 console.log("Five Random Two Digit Number are: "+NumOne+" "+NumTwo+" "+NumThree+" "+NumFour+" "+NumFive);
