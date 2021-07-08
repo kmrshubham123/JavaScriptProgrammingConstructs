b4c5b172d04d6cecb25bb11e4796a5b57dd0e3f9
kmrshubham123 <kmrshubham123@gmail.com>
2021-07-08 18:09:35 +0530

    [Kumar Shubham]Added:Random Dice and Random Digit Program

---
+++ SequenceProblem/RandomDice.js
@@ -0,0 +1,24 @@
+//Generating Random Number of single digit
+let num_1=Math.floor(Math.random()*10);
+console.log("Random Single Digit Number: "+num_1);
+//Dice number between 1 to 6
+let Dice=Math.floor(Math.random()*7);
+console.log("Your Dice Number is: "+Dice);
+//Generating Two Dice number
+let Dice_One=Math.floor(Math.random()*7);
+let Dice_Two=Math.floor(Math.random()*7);
+console.log("First Dice Number is: "+Dice+ " "+"And Second Dice Number is : "+Dice_Two);
+//5 Random 2 digit number
+let NumOne=Math.floor(Math.random()*11);
+let NumTwo=Math.floor(Math.random()*11);
+let NumThree=Math.floor(Math.random()*11);
+let NumFour=Math.floor(Math.random()*11);
+let NumFive=Math.floor(Math.random()*11);
+console.log("Five Random Two Digit Number are: "+NumOne+" "+NumTwo+" "+NumThree+" "+NumFour+" "+NumFive);
+
+
+
+
+
+
+
