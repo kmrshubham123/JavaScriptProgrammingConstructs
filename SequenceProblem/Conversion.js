9505de156ce2edf99b56fa7b6a71981918ffd5e9
kmrshubham123 <kmrshubham123@gmail.com>
2021-07-08 20:16:24 +0530

    [Kumar Shubham]Refactor in Random Method

---
+++ SequenceProblem/Conversion.js
@@ -0,0 +1,5 @@
+//Inch to feet
+var inch = window.prompt("Please Enter the inch(in) value to convert foot(ft))"
+alert("Value is" +inch);
+let ft=( inch / 12 )
+let n=(inch % 12 )
--- SequenceProblem/RandomDice.js
+++ SequenceProblem/RandomDice.js
@@ -1,17 +1,17 @@
-//Generating Random Number of single digit
+//Generating Random Number of single digit *10(0 to 9)
 let num_1=Math.floor(Math.random()*10);
 console.log("Random Single Digit Number: "+num_1);
-//Dice number between 1 to 6
-let Dice=Math.floor(Math.random()*7);
+//Dice number between 1 to 6 (*6)+1=> 1 to 6
+let Dice=Math.floor(Math.random()*6)+1;
 console.log("Your Dice Number is: "+Dice);
-//Generating Two Dice number
-let Dice_One=Math.floor(Math.random()*7);
-let Dice_Two=Math.floor(Math.random()*7);
+//Generating Two Dice number(1,6)
+let Dice_One=Math.floor(Math.random()*6)+1;
+let Dice_Two=Math.floor(Math.random()*6)+1;
 console.log("First Dice Number is: "+Dice_One+ " "+"And Second Dice Number is : "+Dice_Two);
-//5 Random 2 digit number
-let NumOne=Math.floor(Math.random()*11);
-let NumTwo=Math.floor(Math.random()*11);
-let NumThree=Math.floor(Math.random()*11);
-let NumFour=Math.floor(Math.random()*11);
-let NumFive=Math.floor(Math.random()*11);
+//5 Random 2 digit number (*99)+10=>10 to 99 
+let NumOne=Math.floor(Math.random()*99)+10;
+let NumTwo=Math.floor(Math.random()*99)+10;
+let NumThree=Math.floor(Math.random()*99)+10;
+let NumFour=Math.floor(Math.random()*99)+10;
+let NumFive=Math.floor(Math.random()*99)+10;
 console.log("Five Random Two Digit Number are: "+NumOne+" "+NumTwo+" "+NumThree+" "+NumFour+" "+NumFive);
